d3d85f1a8c52bb3fe4d46005668f65cc6a0d37b0
Perrinew <106061214+Perrinew@users.noreply.github.com>
2022-07-11 11:30:48 +0200

    1.3

--- JS_EXO/calcul/calcul.js
+++ JS_EXO/calcul/calcul.js
@@ -1,14 +1,10 @@
-function additionBy()
-    {
-            num1 = document.getElementById("boisson").value;
-            num2 = document.getElementById("plat").value;
-            num3 = document.getElementById("supplément").value;
-            document.getElementById("result").innerHTML = (parseInt (num1) + parseInt (num2) + parseInt (num3));
-    }
-    
-    function divideBy() 
-    { 
-            num1 = document.getElementById("firstNumber").value;
-            num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 / num2;
-    }
+function carre() {
+
+        num1 = document.getElementById("nombre").value;
+
+        document.getElementById("resultat").innerHTML = (`le carré de ${num1} est ${num1 * num1}`);
+        console.log(`le carré de ${num1} est ${num1 * num1}`)
+}
+
+
+
