89eb0da4efb826a60ed35b225a3430cf58265610
Perrinew <106061214+Perrinew@users.noreply.github.com>
2022-07-15 09:44:03 +0200

    exo_dom

---
+++ JS_EXO/dom/dom.js
@@ -0,0 +1,8 @@
+function concatener(c)
+ {
+    nom = document.getElementById("nom").value;
+    prenom = document.getElementById("prenom").value;
+
+    document.getElementById("nom_prenom").value = (`je m'appelle ${nom} ${prenom}`)
+ }
+
--- JS_EXO/test/test.js
+++
@@ -1,6 +0,0 @@
-function checkForm(f)
- {
-    alert("je m'appelle " + f.elements['nom'].value + " " + f.elements['prénom'].value + " j'ai " + f.elements['âge'].value + " ans ");
-    return false; //n'envoie pas le formulaire
- }
-
